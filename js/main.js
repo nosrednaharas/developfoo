ddce4e6ccbc60edec4024ea6b9ede811f4ececf1
Kaitlynn Hendricks <sophistikaty@me.com>
2015-03-22 12:50:11 -0400

    add basic nav structure & header div w possible header object slider to get the cool js going.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
 	console.log('js guild rules');
+
+	var topNav = ["<a target=\"blank\" href=\"https://github.com/JSguild-DC\"><li>Our GitHub</li></a>",
+		"<li>New User Day</li>",
+		"<li>Our favorite JS Resources</li>",
+		"<li>Blog?</li>",
+		"<li>Contact</li>"];
+
+	// Create a function that loads the nav links as scrolling li's when the page loads?
+	function getTopNav (){
+		$('')//
+	};
+
+	/*on page load*/ getTopNav();
 	
 }
+	//do more cool js things. what do you think?
